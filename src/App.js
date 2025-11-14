7d5a812fcbed95db0c792ea06a2b85f949692a20
irons4life <af.spanish.tutor@gmail.com>
2025-11-13 23:50:17 -0500

    ok

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css"
+import Navbar from "./components/navbar/navbar.component"
+const App = () =>{
+  return(
+    <div>
+      <Navbar/>
+      <h1>Startup</h1>
+      <ul>
+        <li>Item1</li>
+        <li>Item2</li>
+        <li>Item3</li>
+      </ul>
+    </div>
+  )
+}
+
+
+
+export default App;
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,10 @@
+import "./navbar.styles.css"
+
+const Navbar = () =>{
+    return(
+    <div>
+        <span>This is the navbar</span>
+    </div>)
+}
+
+export default Navbar;
